60bd37e564c1c2813c6daedc66bad7c3fa7de7e1
kamilo923 <kamilo_vanegas@hotmail.com>
2022-03-23 00:20:29 +0100

    Updates

---
+++ precache-manifest.dd8501cd37d806367712e69ed9f8870f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "440baaa52c6d27932f38",
+    "url": "/React/static/css/main.f1a5bd8a.chunk.css"
+  },
+  {
+    "revision": "440baaa52c6d27932f38",
+    "url": "/React/static/js/main.21615b3b.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "7b7223306c2f40d00263",
+    "url": "/React/static/css/2.e739598e.chunk.css"
+  },
+  {
+    "revision": "7b7223306c2f40d00263",
+    "url": "/React/static/js/2.9e12874e.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1c0a83c618325a5d3d36df8bd201c3c6",
+    "url": "/React/index.html"
+  }
+];
