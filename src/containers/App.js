50390007ce5d7517bf0503aa5a249d9ac32ec586
kamilo923 <kamilo_vanegas@hotmail.com>
2022-05-15 21:27:20 +0200

    Convert class components in functional components with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import '../containers/App.css';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: '',
-		};
-	}
+function App() {
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState('');
 
-	componentDidMount() {
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(users => this.setState({ robots: users }));
-	}
+			.then(users => {
+				setRobots(users);
+			});
+	}, []);
 
-	onSearchChange = event => {
-		this.setState({ searchfield: event.target.value });
+	const onSearchChange = event => {
+		setSearchfield(event.target.value);
 	};
 
-	render() {
-		const { robots, searchfield } = this.state;
+	{
 		const filterRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		});
 
-		return (
-			<>
-				<div className='tc'>
-					<h1 className='f2'>Robofriends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
-					<Scroll>
-						<CardList robots={filterRobots} />
-					</Scroll>
-					<h3 className='f4'>Scroll down for more robots</h3>
-				</div>
-			</>
+		return !robots.length ? (
+			<h1>Loading ...</h1>
+		) : (
+			<div className='tc'>
+				<h1 className='f2'>Robofriends</h1>
+				<SearchBox searchChange={onSearchChange} />
+				<Scroll>
+					<CardList robots={filterRobots} />
+				</Scroll>
+				<h3 className='f4'>Scroll down for more robots</h3>
+			</div>
 		);
 	}
 }
